64a7715fbb6fcab99ff0888fe792ae91ac3b63d2
Neto Nicholas Obinna <neob647@gmail.com>
2020-06-25 10:56:13 +0100

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -31,9 +32,7 @@ let notes = [
     }
 ]
 
-app.get('/', (request , response)=>{
-    response.send('<h1>Hello World!</h1>')
-})
+
 
 
 app.get('/api/notes', (request,response)=>{
